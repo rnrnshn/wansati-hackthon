7d8e257e61d75bc51bf90dddacdb5e5b60038228
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-04-21 13:18:54 +0200

    initial commit

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Wansati Hackthon</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="O Hackathon Wansati é uma iniciativa que visa desenvolver soluções focadas na mulher, olhando para a justiça de gênero, promovendo assim, a inclusão da mulher em áreas de grande impacto em sua vida focando nos pontos acima citados com base no uso da tecnologia." />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Wansati Hackthon</h1>
+    </main>
+  )
+}
