9815d247ac2a2b94450716000af339bfdae02676
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-05-02 08:01:44 +0200

    fix: colors problem on mobile

--- src/app/components/NavItem.tsx
+++ src/app/components/NavItem.tsx
@@ -8,11 +8,11 @@ interface NavItemProps {
 
 const NavItem = ({ title, href, onClickNavItem }: NavItemProps) => {
   return (
-    <li className={`text-base group relative ${ scrollY > 200 ? "text-white" : "text-primary" }`}>
+    <li className={`text-base group relative text-white ${ scrollY > 200 ? "md:text-white" : "md:text-primary" }`}>
       <Link href={ href } onClick={ onClickNavItem }>
         { title }
       </Link>
-      <div className={`absolute w-0 group-hover:w-full h-1 rounded transition-all ${ scrollY > 200 ? "bg-white" : "bg-primary" }`}></div>
+      <div className={`absolute w-0 group-hover:w-full h-1 rounded transition-all bg-white ${ scrollY > 200 ? "md:bg-white" : "md:bg-primary" }`}></div>
     </li>
   )
 }
