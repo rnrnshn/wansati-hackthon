473fd3e9343c3269fdccc9acec20e7983f0555a4
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-05-03 11:32:52 +0200

    fix: use an imported type from data

--- src/app/components/Speaker.tsx
+++ src/app/components/Speaker.tsx
@@ -1,18 +1,12 @@
 import Image from "next/image";
-
-interface Speaker {
-	src: string,
-	alt: string,
-	name: string,
-	role: string
-}
+import type { Speaker } from "../data";
 
 
-const Speaker = ({ src, alt, name, role }: Speaker) => {
+const Speaker = ({ img, alt, name, role }: Speaker) => {
 	return (
 		<div className="justify-center items-center flex flex-col text-center py-5 w-80 h-72 space-y-5">
 			<div className="">
-				<Image className="rounded-full w-48" src={ src } alt={ alt } />
+				<Image className="rounded-full w-40" src={ img } alt={ alt } />
 			</div>
 			<div>
 				<h3 className="font-semibold">{ name }</h3>
